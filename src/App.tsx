411f7620a45292eb775a06260291b4d6abc6878e
Eyob <Eyobeb337@gmail.com>
2025-07-02 12:33:11 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -18,8 +17,7 @@ function App() {
           <Hero />
           <About />
           <Skills />
-          <Projects />
-          <Experience />
+          <Projects />        
           <Contact />
         </main>
         <Footer />
