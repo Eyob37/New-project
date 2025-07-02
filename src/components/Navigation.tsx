89c4d1f625bc0726d0ea9d7c34ca4c537efb2b71
Eyob <Eyobeb337@gmail.com>
2025-07-02 12:34:14 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,8 +11,7 @@ const Navigation: React.FC = () => {
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
-    { name: 'Projects', href: '#projects' },
-    { name: 'Experience', href: '#experience' },
+    { name: 'Projects', href: '#projects' },    
     { name: 'Contact', href: '#contact' },
   ];
 
